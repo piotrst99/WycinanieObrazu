a7f17c3f2c3a28148f79656de5c79c1ef27cefed
piotrstukus@outlook.com <piotrstukus@outlook.com>
2021-09-03 10:38:28 +0200

    Add project files.

---
+++ WycinanieObrazu/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿
+namespace WycinanieObrazu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(210, 120);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(400, 400);
+            this.panel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(476, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "I punkt";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(476, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "II punkt";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(570, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(570, 83);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 15);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "label4";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(210, 120);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ WycinanieObrazu/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WycinanieObrazu
+{
+    public partial class Form1 : Form
+    {
+        private bool click1 = false, click2 = false;
+        private Tuple<int, int> cord1, cord2;
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            /*for(int i=0; i<100; ++i) {
+                for(int j=0; j<100; ++j) {
+                    pictureBox1.BackColor = Color.FromArgb(100,200,124);
+                }
+            }*/
+            pictureBox1.BackColor = Color.FromArgb(100,200,124);
+
+            Bitmap bitmap = new Bitmap(500,500);
+
+            Random r = new Random();
+
+            for(int i=0; i<500; ++i) {
+                for(int j=0; j<500; ++j) {
+                    bitmap.SetPixel(i,j,Color.FromArgb(r.Next(0,255), r.Next(0, 255), r.Next(0, 255)));
+                }
+            }
+
+            panel1.AutoScroll = true;
+            pictureBox1.Image = bitmap;
+
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox2.SizeMode = PictureBoxSizeMode.AutoSize;
+
+            pictureBox2.Hide();
+
+            //pictureBox1.Image 
+            //bitmap = new Bitmap("sciezka");
+            pictureBox1.Image = (Image)bitmap;
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e) {
+            if (e.Button == MouseButtons.Left) {
+                if (!click1) {
+                    label3.Text = e.X.ToString() + " " + e.Y.ToString();
+                    cord1 = new Tuple<int, int>(e.X, e.Y);
+                    click1 = true;
+                }
+                else if (!click2) {
+                    label4.Text = e.X.ToString() + " " + e.Y.ToString();
+                    cord2 = new Tuple<int, int>(e.X, e.Y);
+                    click2 = true;
+                }
+            }
+            
+            if(click1 && click2) {
+                Bitmap bitmap = new Bitmap(500, 500);
+                Random r = new Random();
+                //Bitmap bitmap = new Bitmap(Math.Abs(cord2.Item1 - cord1.Item1 + 1), Math.Abs(cord2.Item2 - cord1.Item2 + 1));
+                /*for (int i=cord1.Item1; i<cord2.Item1; ++i) {
+                    for(int j=cord1.Item2; j<cord2.Item2; ++j) {
+                        //bitmap.SetPixel(i, j, Color.FromArgb(r.Next(0, 255), r.Next(0, 255), r.Next(0, 255)));
+                        //pictureBox1.                
+                        
+                    }
+                }*/
+
+                //pictureBox1.DrawToBitmap(bitmap, pictureBox1.ClientRectangle);
+                //Color color = bitmap.GetPixel(x)
+
+                for (int i = 0; i < 500; ++i) {
+                    for (int j = 0; j < 500; ++j) {
+                        if((i>=cord1.Item1 && i<= cord2.Item1) && (j>=cord1.Item2 && j <= cord2.Item2)) {
+                            //bitmap.SetPixel(i, j, Color.FromArgb(pictureBox1.BackColor.R, pictureBox1.BackColor.G, pictureBox1.BackColor.B));
+                            Color c = ((Bitmap)pictureBox1.Image).GetPixel(i, j);
+                            bitmap.SetPixel(i, j, c);
+                            //bitmap.SetPixel(i, j, Color.FromArgb(r.Next(0, 255), r.Next(0, 255), r.Next(0, 255)));
+                        }
+                        else {
+                            bitmap.SetPixel(i, j, Color.FromArgb(1, 1, 1));
+                        }
+                    }
+                }
+                pictureBox2.Image = bitmap;
+                pictureBox2.Show();
+                pictureBox1.Hide();
+                //pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+
+
+            }
+
+        }
+
+        /*private void pictureBox1_Click(object sender, MouseEventArgs e) {
+            label3.Text = "Clicked";
+            //label3.Text = pictureBox1.Location.X.ToString() + " " + pictureBox1.Location.Y.ToString();
+            if (e.Button == MouseButtons.Left) {
+                label3.Text = e.X.ToString() + " " + e.Y.ToString();
+                //label4.Text = panel1.Width.ToString();
+            }
+                //label3.Text = pictureBox1.ClientRectangle.Width.ToString() + " " + pictureBox1.ClientRectangle.Height.ToString();
+        }*/
+
+
+    }
+}
